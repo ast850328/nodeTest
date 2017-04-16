2ff6ff1528f0ef8211b4e62cd6e9f531d7b4b1d5
Sam-TSAI <ast850328@gmail.com>
2017-04-16 22:52:22 +0800

    add nodeTest

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var request = require('request');
+
+http.createServer(function(req, res) {
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    //res.setHeader('Content-Type', 'text/html');
+    request.post(
+		{
+			url:'http://ycchen.im.ncnu.edu.tw/join.php', 
+			form: {
+				Yuer: 'handsome'
+			}
+		}, function(err, httpResponse, body){
+			res.write(body);
+			res.end(); 
+		}
+	);
+}).listen(7774);
